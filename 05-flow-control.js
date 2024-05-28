85baa92a67957c573dd303103ca1cfd978ab4ff7
Chalry_KR <cjyang.cjy@gmail.com>
2024-05-28 16:33:28 +0900

    240528

--- 05-flow-control.js
+++ 05-flow-control.js
@@ -58,3 +58,19 @@ while (i > 0) {
     i -= 1;
 }
 
+//  for ... in : 객체의 속성 키를 반복
+const obj = {
+    name: "홍길동",
+    age: 28,
+    job: "도적"
+}
+
+for (let key in obj) {
+    console.log(`${key} -> ${obj[key]}`);
+}
+
+//  for ... of : 배열을 순회
+const arr = [1,2,3,4,5];
+for (let item of arr) {
+    console.log(item);
+}
